f35df65181ecbcbb4598cd7b359ee4afa827cf83
Anders <wenhaug@stud.ntnu.no>
2014-02-25 16:13:13 +0100

    Added Message and derived classes. Updated client with simple testing code.

---
+++ Message.py
@@ -0,0 +1,135 @@
+__author__ = 'anders'
+
+import json
+
+
+class MessageException(Exception):
+    pass
+
+
+class Message(object):
+    def __init__(self):
+        self.message_attributes = {}
+        self.complete = False
+
+    def completeGuard(self):
+        if self.complete:
+            raise MessageException("Message already complete!")
+
+    def pack_JSON(self):
+        if not self.complete:
+            raise MessageException("Attempted to pack unfinished message!")
+
+        return json.dumps(self.message_attributes)
+
+
+class LoginRequestMessage(Message):
+    def __init__(self):
+        super(LoginRequestMessage, self).__init__()
+        self.message_attributes["request"] = "login"
+
+    def set_login_info(self, username):
+        self.completeGuard()
+        self.message_attributes["username"] = username
+        self.complete = True
+
+
+class LoginResponseMessage(Message):
+    def __init__(self):
+        super(LoginResponseMessage, self).__init__()
+        self.message_attributes["response"] = "login"
+
+    def set_success(self, username, message_log):
+        self.completeGuard()
+        self.message_attributes["username"] = username
+        self.message_attributes["messages"] = message_log
+        self.complete = True
+
+    def set_invalid_username(self, username):
+        self.completeGuard()
+        self.message_attributes["username"] = username
+        self.message_attributes["error"] = "Invalid username!"
+        self.complete = True
+
+    def set_taken_username(self, username):
+        self.completeGuard()
+        self.message_attributes["username"] = username
+        self.message_attributes["error"] = "Name already taken!"
+        self.complete = True
+
+
+class ChatRequestMessage(Message):
+    def __init__(self):
+        super(ChatRequestMessage, self).__init__()
+        self.message_attributes["request"] = "message"
+
+    def set_chat_message(self, message):
+        self.completeGuard()
+        self.message_attributes["message"] = message
+        self.complete = True
+
+
+class ChatResponseMessage(Message):
+    def __init__(self):
+        super(ChatResponseMessage, self).__init__()
+        self.message_attributes["response"] = "message"
+
+    def set_success(self, message):
+        self.completeGuard()
+        self.message_attributes["message"] = message
+        self.complete = True
+
+    def set_not_logged_in(self):
+        self.completeGuard()
+        self.message_attributes["error"] = "You are not logged in!"
+        self.complete = True
+
+
+class LogoutRequestMessage(Message):
+    def __init__(self):
+        super(LogoutRequestMessage, self).__init__()
+        self.message_attributes["request"] = "logout"
+        self.complete = True
+
+
+class LogoutResponseMessage(Message):
+    def __init__(self):
+        super(LogoutResponseMessage, self).__init__()
+        self.message_attributes["response"] = "logout"
+
+    def set_success(self, username):
+        self.completeGuard()
+        self.message_attributes["username"] = username
+        self.complete = True
+
+    def set_not_logged_in(self, username):
+        self.completeGuard()
+        self.message_attributes["username"] = username
+        self.message_attributes["error"] = "Not logged in!"
+        self.complete = True
+
+
+
+
+# Testing
+if __name__ == "__main__":
+    print "Testing LoginRequestMessage"
+    loginRequestMessage = LoginRequestMessage()
+
+    try:
+        loginRequestMessage.pack_JSON()
+    except MessageException, e:
+        print "Guard test #1 successful"
+    else:
+        print "Guard test #1 unsuccessful"
+
+    loginRequestMessage.set_login_info("anders")
+
+    try:
+        loginRequestMessage.set_login_info("anders")
+    except MessageException, e:
+        print "Guard test #2 successful"
+    else:
+        print "Guard test #2 unsuccessful"
+
+    print "LoginRequestMessage: " + loginRequestMessage.pack_JSON()
--- client.py
+++ client.py
@@ -2,25 +2,33 @@
 KTN-project 2013 / 2014
 '''
 import socket
+from Message import *
 
 
 class Client(object):
-
-    def __init__(self):
+    def __init__(self, host, port):
         self.connection = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-    def start(self, host, port):
         self.connection.connect((host, port))
-        self.send('Hello')
-        received_data = self.connection.recv(1024).strip()
-        print 'Received from server: ' + received_data
-        self.connection.close()
+
+    def start(self):
+        while not self.login():
+            continue
 
     def message_received(self, message, connection):
         pass
 
     def connection_closed(self, connection):
         pass
+
+    def login(self):
+        self.username = raw_input("Select username: ")
+        loginRequestMessage = LoginRequestMessage()
+        loginRequestMessage.set_login_info(self.username)
+        network_data = loginRequestMessage.pack_JSON()
+        self.send(network_data)
+        print self.connection.recv(1024).strip()
+        self.connection.close()
 
     def send(self, data):
         self.connection.sendall(data)
@@ -30,5 +38,5 @@ class Client(object):
 
 
 if __name__ == "__main__":
-    client = Client()
-    client.start('localhost', 9999)
+    client = Client('localhost', 9999)
+    client.login()
