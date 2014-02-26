42b74d58731d137152671a242afb6f79f885ec07
Anders <wenhaug@stud.ntnu.no>
2014-02-26 16:29:14 +0100

    Removed unnecessary comment.

--- MessageWorker.py
+++ MessageWorker.py
@@ -1,18 +1,3 @@
-'''
-KTN-project 2013 / 2014
-Python daemon thread class for listening for events on
-a socket and notifying a listener of new messages or
-if the connection breaks.
-
-A python thread object is started by calling the start()
-method on the class. in order to make the thread do any
-useful work, you have to override the run() method from
-the Thread superclass. NB! DO NOT call the run() method
-directly, this will cause the thread to block and suspend the
-entire calling process' stack until the run() is finished.
-it is the start() method that is responsible for actually
-executing the run() method in a new thread.
-'''
 from threading import Thread
 import json
 
@@ -22,6 +7,7 @@ class MessageWorker(Thread):
         super(MessageWorker, self).__init__(name="Listener")
         self.connection = connection
         self.client = client
+        self.daemon = True
 
     def run(self):
         run = True
@@ -31,6 +17,8 @@ class MessageWorker(Thread):
 
             # Decode and send data to client
             if json_data:
+                #print "Received data from server: " + json_data
+
                 data = json.loads(json_data)
                 self.send_data(data)
 
