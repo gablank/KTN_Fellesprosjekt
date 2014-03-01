1106f8f85a80dce68f429b129d81e2f230937598
Anders Wenhaug <andersw@broadpark.no>
2014-03-01 16:10:13 +0100

    Added lines for deploying both master and Experimental branches.

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,14 @@
 SERVER="furic.pw"
 USER="anders"
 PORT="45118"
-DIRECTORY="/home/anders/"
+DIRECTORY="/home/anders/KTN_Fellesprosjekt/"
 
-scp -P $PORT server.py $USER@$SERVER:$DIRECTORY
-scp -P $PORT Message.py $USER@$SERVER:$DIRECTORY
+git checkout master
+
+scp -P $PORT server.py $USER@$SERVER:$DIRECTORY"master/"
+scp -P $PORT Message.py $USER@$SERVER:$DIRECTORY"master/"
+
+git checkout Experimental
+
+scp -P $PORT server.py $USER@$SERVER:$DIRECTORY"Experimental/server.py"
+scp -P $PORT Message.py $USER@$SERVER:$DIRECTORY"Experimental/Message.py"
