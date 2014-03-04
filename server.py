9b5669b3d9a0deb4a37b39de5f4a4aee44c3e62f
Anders Wenhaug <andersw@broadpark.no>
2014-03-04 23:12:03 +0100

    Changed threads to daemon.

--- server.py
+++ server.py
@@ -346,6 +346,7 @@ if __name__ == "__main__":
 
     # Create the server, binding to localhost on port 9999
     server = ThreadedTCPServer((HOST, PORT), ClientHandler)
+    server.daemon_threads = True
 
     # Activate the server; this will keep running until you
     # interrupt the program with Ctrl-C
